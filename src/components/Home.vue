e5d7754b28cfb64f98c0b8ef9bcd93ca2e0ab09e
Moshiur Rahman <unimrgm@gmail.com>
2022-02-19 22:15:03 +0600

    added input fields

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,29 @@
 <template>
-  <div class="bg-hero container font-josefin">
-    <h1 class="text-xl">Hellow</h1>
+  <div class="font-josefin">
+    <!-- Header -->
+    <header
+      class="bg-hero bg-no-repeat bg-cover pt-[48px] pb-[132px] sm:pt-[70px] sm:pb-[180px]"
+    >
+      <div class="flex items-center container">
+        <h1 class="text-white tracking-[10px] flex-1 text-xl sm:text-4xl">
+          TODO
+        </h1>
+        <img src="../assets/img/icon-moon.svg" alt="" />
+      </div>
+    </header>
+    <!-- Add Todo -->
+    <section class="container relative">
+      <div class="absolute top-[-150px] w-full">
+        <div class="flex items-center relative">
+          <input class="absolute left-4 h-full" type="checkbox" />
+          <input
+            class="w-full py-5 px-10 text-white placeholder:text-gray-400 bg-light-grayish-blue rounded-md focus:outline-none border-2 border-solid border-transparent"
+            type="text"
+            placeholder="Add Todo"
+          />
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,15 @@ module.exports = {
       fontFamily: {
         josefin: ['Josefin Sans', 'sans-serif'],
       },
+      colors: {
+        'dark-blue': 'hsl(235, 21%, 11%)',
+        'dark-desaturated-blue': 'hsl(235, 24%, 19%)',
+        'light-grayish-blue': 'hsl(234, 39%, 85%)',
+        'light-grayish-blue-hover': 'hsl(236, 33%, 92%)',
+        'dark-grayish-blue': 'hsl(234, 11%, 52%)',
+        'very-dark-grayish-blue': 'hsl(233, 14%, 35%)',
+        // - Very Dark Grayish Blue: hsl(237, 14%, 26%)
+      },
     },
   },
   plugins: [],
